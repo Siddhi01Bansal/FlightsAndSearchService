c29d3e8dc7c98a5f64e0dc61a0f91047db72dcaf
Siddhi Bansal <siddhi.2003.bansal@gmail>
2023-09-21 15:39:37 +0530

    Added filters to getAllCities Api and airports model

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -59,8 +59,18 @@ class CityRepository{
         }
     }
 
-    async getAllCities() { //filter can be empty also
+    async getAllCities(filter) { //filter can be empty also
         try {
+            if(filter.name){
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities = await City.findAll();
             return cities;
         } catch (error) {
